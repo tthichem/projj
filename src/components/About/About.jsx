b18a6e119444b54de622f52431d505a6ddc7c198
tthichem <thiicheem@gmail.com>
2025-05-10 21:53:49 +0100

    complete homepage

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,14 +15,14 @@ const About = ({theme}) => {
       good deed. By using our skills to help the university community, we hope this work will be 
       useful for students, teachers, and visitors for a long time.
       The site was fully developed with React, using modern tools 
-      and simple, accessible, and scalable design principles. It shows our desire
-      to contribute positively to our university and leave something helpful and open for future generations.<br/>
-      If you notice a bug, a display issue, or if you have a suggestion, report it here - it helps us to improve the site and make it even better for everyone!
+      and simple design principles. It shows our desire
+      to contribute positively to our university and leave something helpful.<br/>
+      If you notice a bug, a display issue, or if you have a suggestion, report it here, it helps us to improve the site and make it even better.
       </p>
       <button className='report-button' onClick={() => setShowReport(true)}>Report a problem</button>
       {showReport && <ReportsPopUp onClose={() => setShowReport(false)} theme={theme}/>}
       <div style={{marginTop: 24, textAlign: 'center'}}>
-        <span style={{fontSize: '1rem', color: '#666'}}>and you can take a look here...</span>
+        <span style={{fontSize: '1rem', color: '#666'}}>you can also take a look here...</span>
         <br/>
         <div style={{display:'flex', justifyContent:'center', gap:'18px', marginTop:8}}>
           <a href="https://github.com/abdelhak46/prj" target="_blank" rel="noopener noreferrer" style={{display:'inline-block'}}>
